9f6a2eb04d563d5f8176f0fb8003dc0ad30585ae
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-15 11:49:15 +0100

    useEf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,39 +8,44 @@ import { AppStyled } from "App.Styled";
 
 export function App() {
   const [card, setCard] = useState([]);
-  const [inputValue, setInputValue] = useState(null),
-  const [page, setPage] = useState(1),
+  const [inputValue, setInputValue] = useState(null);
+  const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [showBtn] = useState(false);
 
   useEffect(() => {
-    setStatus(isLoading.true)
+    setIsLoading(isLoading.true)
 
+         getImages(inputValue, page)
+        .then(cards =>
+          setCard(prev => ({
+            card: [...prev.card, ...cards.hits],
+          showBtn: page < Math.ceil(cards.totalHits / 12)
+        })))
       .catch(error => {
-        setError(error);
-        setStatus()
+        setPage(error)
       .finally(() => {
         isLoading.false
       })
     })
-  });
+  }, [inputValue, page]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.page !== this.state.page ||
-      prevState.inputValue !== this.state.inputValue) {
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.page !== this.state.page ||
+  //     prevState.inputValue !== this.state.inputValue) {
 
-      this.setState({ isLoading: true });
+  //     this.setState({ isLoading: true });
 
-      getImages(this.state.inputValue, this.state.page)
-        .then(cards =>
-          this.setState(prev => ({
-            card: [...prev.card, ...cards.hits],
-          showBtn: this.state.page < Math.ceil(cards.totalHits / 12)
-        })))
-        .catch(error => console.log(error))
-        .finally(()=> this.setState({ isLoading: false}));
-    }
-  };
+  //     getImages(this.state.inputValue, this.state.page)
+  //       .then(cards =>
+  //         this.setState(prev => ({
+  //           card: [...prev.card, ...cards.hits],
+  //         showBtn: this.state.page < Math.ceil(cards.totalHits / 12)
+  //       })))
+  //       .catch(error => console.log(error))
+  //       .finally(()=> this.setState({ isLoading: false}));
+  //   }
+  // };
 
   findImage = e => {
     this.setState({ card: [], page: 1, inputValue: e.search });
